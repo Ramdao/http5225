96d10f5bbe0026ae334fea9141bbba040a04d417
Tashrif Radin Ali <radin00@hotmail.com>
2025-01-21 17:56:45 -0500

    week2

---
+++ week2/in-class.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week 2</title>
+</head>
+<body>
+<!-- Javascript -->
+<script>
+    document.write("Hellow world in Javascript!")
+    </script>
+<!-- PHP -->
+<?php
+  
+echo 'Hello world to PHP!';
+
+?>
+<?php
+
+ echo 'Welcome to Gary\'s World';
+
+?>
+</body>
+</html>
--- week2/index.php
+++ week2/index.php
@@ -1,23 +1,44 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Week 2</title>
-</head>
-<body>
-
-<script>
-    document.write("Hellow world in Javascript!")
-    </script>
-<?php
+<!doctype html>
+<html>
+  <head>
+    
+    <title>PHP and Creating Output</title>
+    
+  </head>
+  <body>
   
-echo 'Hello world to PHP!';
+    <?php 
+    echo '<h1>PHP and Creating Output</h1>'; 
+    echo '<p>The PHP echo command can be used to create output.</p>'
 
-?>
-<?php
- echo 'Welcome to Gary\'s World';
-?>
-</body>
+    ?>
+
+    <p>When creating output using echo and PHP, quotes can often cause problems. There are several solutions to using quotes within an echo statement:</p>
+    
+    <ul>
+        <li>Use HTML special characters</li>
+        <li>Alternate between single and double quotes</li>
+        <li>Use a backslash to escape quotes</li>
+    </ul>
+    
+    <h2>More HTML to Convert</h2>
+
+    <p>PHP says "Hello World!"</p>
+
+    <p>Can you display a sentence with ''' and """?</p>
+
+    <img src="php-logo.png">
+
+    <?php
+    echo '<img src="https://google.com/image">';
+    ?>
+    <img src ="<?php echo 'https://google.com/image' ?>" alt="<?php echo 'ALT TAG'?>">
+
+    <?php
+    $name = "Tashrif Radin";
+    $lastName = 'Ali';
+
+    echo 'Hello,' . $name . ' '. $lastName;
+    ?>
+  </body>
 </html>
-
